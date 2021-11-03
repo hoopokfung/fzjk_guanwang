349700b17de0a6cdba539693edb11263186c1854
hoopokfung <49724593+hoopokfung@users.noreply.github.com>
2021-11-03 16:30:35 +0800

    first commit

---
+++ src/test/java/com/fzjk/guanwang/GuanwangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzjk.guanwang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuanwangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
